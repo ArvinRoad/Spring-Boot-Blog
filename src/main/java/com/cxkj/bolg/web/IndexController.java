2e9d6fbfa313d1c4d05676d82c29177900e8027c
ArvinRoad <53329456+ArvinRoad@users.noreply.github.com>
2021-02-04 04:51:49 +0800

    日志处理

--- src/main/java/com/cxkj/bolg/web/IndexController.java
+++ src/main/java/com/cxkj/bolg/web/IndexController.java
@@ -3,18 +3,20 @@ package com.cxkj.bolg.web;
 import com.cxkj.bolg.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  *  Created by Arvin on 2021/2/3.
  */
 @Controller
 public class IndexController {
-    @GetMapping("/")
-    public String index(){
-        String bolg = null;
-        if (bolg == null){
-            throw  new NotFoundException("博客不存在");
-        }
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id,@PathVariable String name){
+            /*String bolg = null;
+            if (bolg == null){
+                throw  new NotFoundException("博客不存在");
+            }*/
+        System.out.println("------ Index ------");
         return "index";
     }
 
